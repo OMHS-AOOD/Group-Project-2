e07c29a2316a2c259244e83a574b73915c32483b
jfreem9916 <jonahwfreeman@yahoo.com>
2017-02-28 11:12:16 -0500

    player

--- AOOD_GroupProject2/src/Player.java
+++ AOOD_GroupProject2/src/Player.java
@@ -1,4 +1,13 @@
 
 public class Player {
-
-}
+	private String name;
+	private CardStack battle, speed, distance, safety, hand;
+	public Player(String n){
+		name = n;
+		battle = new CardStack();
+		speed = new CardStack();
+		distance = new CardStack();
+		safety = new CardStack();
+		hand = new CardStack();
+	}
+}	
