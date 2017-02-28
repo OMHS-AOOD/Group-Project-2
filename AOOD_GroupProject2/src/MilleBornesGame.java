9e147d8cbd5f485472ee13de6f812a80244dac71
jfreem9916 <jonahwfreeman@yahoo.com>
2017-02-28 11:06:59 -0500

    asdasdasd

---
+++ AOOD_GroupProject2/src/MilleBornesGame.java
@@ -0,0 +1,9 @@
+
+public class MilleBornesGame {
+	private Deck deck;
+	private CardStack discard, battle, speed, distance, safety;
+	public MilleBornesGame(){
+		deck = new Deck();
+		discard = new CardStack();
+	}
+}
